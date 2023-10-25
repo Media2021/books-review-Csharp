3b1e308ca09ade89fdd32798cbdc24cc545b7001
Media <95234709+Media2021@users.noreply.github.com>
2023-10-25 16:34:46 +0200

    Update BooksDB.cs

--- DBlayer/BooksDB.cs
+++ DBlayer/BooksDB.cs
@@ -9,7 +9,7 @@ namespace DBlayer
     public class BooksDB
     {
 
-        SqlConnection conn = new SqlConnection("server  = mssqlstud.fhict.local;database= dbi499087;User Id = dbi499087; Password=2018Ayham.; ");
+        SqlConnection conn = new SqlConnection("server  = mssqlstud.fhict.local;database= mydatabase;User Id = myId; Password=myPassword; ");
 
     
 
